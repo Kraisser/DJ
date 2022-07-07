96367b82da78ace91335efb17711da81b34042c9
Kraisser <vgaiko@mail.ru>
2022-07-07 23:25:20 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import 'normalize.css';
+import '../css/variables.css';
+
+import '../css/index.css';
+import '../css/header.css';
+import '../css/main-start-view.css';
+import '../css/news.css';
+import '../css/about-us.css';
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+	mode: 'development',
+	devtool: 'source-map',
+	devServer: {
+		historyApiFallback: true,
+		static: path.resolve(__dirname, './build'),
+		open: true,
+		compress: true,
+		hot: false,
+		port: 8080,
+		client: {
+			overlay: {
+				errors: true,
+				warnings: false,
+			},
+			progress: true,
+		},
+	},
+	entry: {
+		main: path.resolve(__dirname, './src/js/index.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: '[name].bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: 'html-loader',
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				exclude: /node_modules/,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				exclude: /node_modules/,
+				type: 'asset/inline',
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{loader: 'style-loader'},
+					{loader: 'css-loader'},
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								sourceMap: true,
+								plugins: [require('autoprefixer')],
+							},
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: '[title]',
+			template: './src/index.html',
+		}),
+	],
+};
