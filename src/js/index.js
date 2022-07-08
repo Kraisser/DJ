bd088befaf404457493e18208d004c16c538231e
Kraisser <vgaiko@mail.ru>
2022-07-08 19:11:50 +0300

    1.1 layout end

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,7 @@ import '../css/header.css';
 import '../css/main-start-view.css';
 import '../css/news.css';
 import '../css/about-us.css';
+import '../css/last-track.css';
+import '../css/gallery.css';
+import '../css/slider.css';
+import '../css/footer.css';
